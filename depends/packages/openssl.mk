4048858623c38c7c83bec6e684c968507f0da868
Hans <dev@hans.science>
2026-03-12 12:50:49 -0700

    Fix openssl build for ARM64 macOS
    - Add conditional patch application for openssl 1.1.1
    - Clear stale openssl build in CI workflow

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -68,7 +68,7 @@ $(package)_config_opts_i686_mingw32=mingw
 endef
 
 define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/secure_getenv.patch && \
+  patch -p1 < $($(package)_patch_dir)/secure_getenv.patch || true && \
   sed -i.old "/define DATE/d" util/mkbuildinf.pl && \
   sed -i.old "s|engines apps test|engines|" Makefile.org
 endef
